c987af799ef581394893a94fbc85187988a139d1
smolga2110 <73380083+smolga2110@users.noreply.github.com>
2020-10-27 14:25:07 +0300

    Fixed an incompatibility Issue

---
+++ ui_mainwindow.py
@@ -0,0 +1,160 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'mainwindow.ui'
+##
+## Created by: Qt User Interface Compiler version 5.15.1
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+from PySide2.QtWidgets import *
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(539, 433)
+        MainWindow.setStyleSheet(u"background-color: rgb(143, 143, 143);")
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.centralwidget.setStyleSheet(u"QTabWidget {\n"
+"	color:rgb(0,0,0);\n"
+"	background-color:rgb(247,246,246);\n"
+"}\n"
+"QTabWidget::pane {\n"
+"		border-color: rgb(77,77,77);\n"
+"		background-color:rgb(101,101,101);\n"
+"		border-style: solid;\n"
+"		border-width: 1px;\n"
+"    	border-radius: 6px;\n"
+"}")
+        self.plainTextEdit = QPlainTextEdit(self.centralwidget)
+        self.plainTextEdit.setObjectName(u"plainTextEdit")
+        self.plainTextEdit.setGeometry(QRect(13, 13, 511, 231))
+        self.plainTextEdit.setStyleSheet(u"QPlainTextEdit {\n"
+"	background-color:rgb(42, 42, 42);\n"
+"	color: rgb(0, 255, 0);\n"
+"}")
+        self.plainTextEdit.setReadOnly(True)
+        self.lineEdit = QLineEdit(self.centralwidget)
+        self.lineEdit.setObjectName(u"lineEdit")
+        self.lineEdit.setGeometry(QRect(10, 250, 511, 41))
+        self.lineEdit.setStyleSheet(u"QLineEdit {\n"
+"	border-width: 1px; border-radius: 4px;\n"
+"	border-color: rgb(58, 58, 58);\n"
+"	border-style: inset;\n"
+"	padding: 0 8px;\n"
+"	color: rgb(255, 255, 255);\n"
+"	background:rgb(100, 100, 100);\n"
+"	selection-background-color: rgb(187, 187, 187);\n"
+"	selection-color: rgb(60, 63, 65);\n"
+"}")
+        self.lineEdit.setMaxLength(32767)
+        self.pushButton = QPushButton(self.centralwidget)
+        self.pushButton.setObjectName(u"pushButton")
+        self.pushButton.setGeometry(QRect(10, 300, 511, 51))
+        self.pushButton.setStyleSheet(u"QPushButton{\n"
+"	border-style: outset;\n"
+"	border-width: 2px;\n"
+"	border-top-color: qlineargradient(spread:pad, x1:0.5, y1:0.6, x2:0.5, y2:0.4, stop:0 rgba(115, 115, 115, 255), stop:1 rgba(62, 62, 62, 255));\n"
+"	border-right-color: qlineargradient(spread:pad, x1:0.4, y1:0.5, x2:0.6, y2:0.5, stop:0 rgba(115, 115, 115, 255), stop:1 rgba(62, 62, 62, 255));\n"
+"	border-left-color: qlineargradient(spread:pad, x1:0.6, y1:0.5, x2:0.4, y2:0.5, stop:0 rgba(115, 115, 115, 255), stop:1 rgba(62, 62, 62, 255));\n"
+"	border-bottom-color: rgb(58, 58, 58);\n"
+"	border-bottom-width: 1px;\n"
+"	border-style: solid;\n"
+"	color: rgb(255, 255, 255);\n"
+"	padding: 2px;\n"
+"	background-color: qlineargradient(spread:pad, x1:0.5, y1:1, x2:0.5, y2:0, stop:0 rgba(77, 77, 77, 255), stop:1 rgba(97, 97, 97, 255));\n"
+"}\n"
+"QPushButton:hover{\n"
+"	border-style: outset;\n"
+"	border-width: 2px;\n"
+"	border-top-color: qlineargradient(spread:pad, x1:0.5, y1:0.6, x2:0.5, y2:0.4, stop:0 rgba(180, 180, 180, 255), stop:1 rgba(110, 110, 110, 255"
+                        "));\n"
+"	border-right-color: qlineargradient(spread:pad, x1:0.4, y1:0.5, x2:0.6, y2:0.5, stop:0 rgba(180, 180, 180, 255), stop:1 rgba(110, 110, 110, 255));\n"
+"	border-left-color: qlineargradient(spread:pad, x1:0.6, y1:0.5, x2:0.4, y2:0.5, stop:0 rgba(180, 180, 180, 255), stop:1 rgba(110, 110, 110, 255));\n"
+"	border-bottom-color: rgb(115, 115, 115);\n"
+"	border-bottom-width: 1px;\n"
+"	border-style: solid;\n"
+"	color: rgb(255, 255, 255);\n"
+"	padding: 2px;\n"
+"	background-color: qlineargradient(spread:pad, x1:0.5, y1:1, x2:0.5, y2:0, stop:0 rgba(107, 107, 107, 255), stop:1 rgba(157, 157, 157, 255));\n"
+"}\n"
+"QPushButton:pressed{\n"
+"	border-style: outset;\n"
+"	border-width: 2px;\n"
+"	border-top-color: qlineargradient(spread:pad, x1:0.5, y1:0.6, x2:0.5, y2:0.4, stop:0 rgba(62, 62, 62, 255), stop:1 rgba(22, 22, 22, 255));\n"
+"	border-right-color: qlineargradient(spread:pad, x1:0.4, y1:0.5, x2:0.6, y2:0.5, stop:0 rgba(115, 115, 115, 255), stop:1 rgba(62, 62, 62, 255));\n"
+"	border-left-color: qlineargradient(spr"
+                        "ead:pad, x1:0.6, y1:0.5, x2:0.4, y2:0.5, stop:0 rgba(115, 115, 115, 255), stop:1 rgba(62, 62, 62, 255));\n"
+"	border-bottom-color: rgb(58, 58, 58);\n"
+"	border-bottom-width: 1px;\n"
+"	border-style: solid;\n"
+"	color: rgb(255, 255, 255);\n"
+"	padding: 2px;\n"
+"	background-color: qlineargradient(spread:pad, x1:0.5, y1:1, x2:0.5, y2:0, stop:0 rgba(77, 77, 77, 255), stop:1 rgba(97, 97, 97, 255));\n"
+"}")
+        self.pushButton_2 = QPushButton(self.centralwidget)
+        self.pushButton_2.setObjectName(u"pushButton_2")
+        self.pushButton_2.setGeometry(QRect(10, 360, 511, 51))
+        self.pushButton_2.setStyleSheet(u"QPushButton{\n"
+"	border-style: outset;\n"
+"	border-width: 2px;\n"
+"	border-top-color: qlineargradient(spread:pad, x1:0.5, y1:0.6, x2:0.5, y2:0.4, stop:0 rgba(115, 115, 115, 255), stop:1 rgba(62, 62, 62, 255));\n"
+"	border-right-color: qlineargradient(spread:pad, x1:0.4, y1:0.5, x2:0.6, y2:0.5, stop:0 rgba(115, 115, 115, 255), stop:1 rgba(62, 62, 62, 255));\n"
+"	border-left-color: qlineargradient(spread:pad, x1:0.6, y1:0.5, x2:0.4, y2:0.5, stop:0 rgba(115, 115, 115, 255), stop:1 rgba(62, 62, 62, 255));\n"
+"	border-bottom-color: rgb(58, 58, 58);\n"
+"	border-bottom-width: 1px;\n"
+"	border-style: solid;\n"
+"	color: rgb(255, 255, 255);\n"
+"	padding: 2px;\n"
+"	background-color: qlineargradient(spread:pad, x1:0.5, y1:1, x2:0.5, y2:0, stop:0 rgba(77, 77, 77, 255), stop:1 rgba(97, 97, 97, 255));\n"
+"}\n"
+"QPushButton:hover{\n"
+"	border-style: outset;\n"
+"	border-width: 2px;\n"
+"	border-top-color: qlineargradient(spread:pad, x1:0.5, y1:0.6, x2:0.5, y2:0.4, stop:0 rgba(180, 180, 180, 255), stop:1 rgba(110, 110, 110, 255"
+                        "));\n"
+"	border-right-color: qlineargradient(spread:pad, x1:0.4, y1:0.5, x2:0.6, y2:0.5, stop:0 rgba(180, 180, 180, 255), stop:1 rgba(110, 110, 110, 255));\n"
+"	border-left-color: qlineargradient(spread:pad, x1:0.6, y1:0.5, x2:0.4, y2:0.5, stop:0 rgba(180, 180, 180, 255), stop:1 rgba(110, 110, 110, 255));\n"
+"	border-bottom-color: rgb(115, 115, 115);\n"
+"	border-bottom-width: 1px;\n"
+"	border-style: solid;\n"
+"	color: rgb(255, 255, 255);\n"
+"	padding: 2px;\n"
+"	background-color: qlineargradient(spread:pad, x1:0.5, y1:1, x2:0.5, y2:0, stop:0 rgba(107, 107, 107, 255), stop:1 rgba(157, 157, 157, 255));\n"
+"}\n"
+"QPushButton:pressed{\n"
+"	border-style: outset;\n"
+"	border-width: 2px;\n"
+"	border-top-color: qlineargradient(spread:pad, x1:0.5, y1:0.6, x2:0.5, y2:0.4, stop:0 rgba(62, 62, 62, 255), stop:1 rgba(22, 22, 22, 255));\n"
+"	border-right-color: qlineargradient(spread:pad, x1:0.4, y1:0.5, x2:0.6, y2:0.5, stop:0 rgba(115, 115, 115, 255), stop:1 rgba(62, 62, 62, 255));\n"
+"	border-left-color: qlineargradient(spr"
+                        "ead:pad, x1:0.6, y1:0.5, x2:0.4, y2:0.5, stop:0 rgba(115, 115, 115, 255), stop:1 rgba(62, 62, 62, 255));\n"
+"	border-bottom-color: rgb(58, 58, 58);\n"
+"	border-bottom-width: 1px;\n"
+"	border-style: solid;\n"
+"	color: rgb(255, 255, 255);\n"
+"	padding: 2px;\n"
+"	background-color: qlineargradient(spread:pad, x1:0.5, y1:1, x2:0.5, y2:0, stop:0 rgba(77, 77, 77, 255), stop:1 rgba(97, 97, 97, 255));\n"
+"}")
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.plainTextEdit.setPlainText("")
+        self.lineEdit.setInputMask("")
+        self.lineEdit.setText("")
+        self.pushButton.setText(QCoreApplication.translate("MainWindow", u"\u0412\u044b\u0431\u0440\u0430\u0442\u044c \u041f\u0430\u043f\u043a\u0443", None))
+        self.pushButton_2.setText(QCoreApplication.translate("MainWindow", u"\u0421\u043a\u0430\u0447\u0430\u0442\u044c \u0412\u0438\u0434\u0435\u043e", None))
+    # retranslateUi
+
